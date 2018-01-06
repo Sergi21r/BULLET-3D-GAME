b9b37c0b670a1037161f06df47ad22d09337cf0e
Sergi21r <sergi21r@gmail.com>
2018-01-06 21:09:46 +0100

    map done

--- RACING GAME/ModuleSceneIntro.cpp
+++ RACING GAME/ModuleSceneIntro.cpp
@@ -39,7 +39,7 @@ bool ModuleSceneIntro::Start()
 	f10 = App->physics->AddCube(10, 1, 20, -18, 2, -50, 0.0f, Grey);
 	f11 = App->physics->AddCubeRotY(10, 1, 10, -19, 2, -37, 0.0f, Grey, 160);
 	f12 = App->physics->AddCubeRotY(10, 1, 10, -21, 2, -34, 0.0f, Grey, 140);
-	f13 = App->physics->AddCubeRotY(10, 1, 10, -23, 2, -31, 0.0f, Grey, 120);
+	f13 = App->physics->AddCubeRotY(10, 1, 10, -23, 2, -31, 0.0f, Grey, 130);
 	f14 = App->physics->AddCube(20, 1, 10, -33, 2, -29, 0.0f, Grey);
 	f15 = App->physics->AddCubeRotY(10, 1, 10, -45, 2, -28, 0.0f, Grey, 120);
 	f16 = App->physics->AddCubeRotY(10, 1, 10, -48, 2, -26, 0.0f, Grey, 140);
@@ -51,7 +51,7 @@ bool ModuleSceneIntro::Start()
 	f22 = App->physics->AddCube(20, 1, 10, -33, 2, 24, 0.0f, Grey);
 	f23 = App->physics->AddCubeRotY(10, 1, 10, -22, 2, 25, 0.0f, Grey, 160);
 	f24 = App->physics->AddCubeRotY(10, 1, 10, -19, 2, 27, 0.0f, Grey, 140);
-	f25 = App->physics->AddCubeRotY(10, 1, 10, -16, 2, 29, 0.0f, Grey, 120);
+	f25 = App->physics->AddCubeRotY(10, 1, 10, -16, 2, 29, 0.0f, Grey, 130);
 	f26 = App->physics->AddCube(10, 1, 40, -14, 2, 51, 0.0f, Grey);
 	f27 = App->physics->AddCubeRotY(10, 1, 10, -13, 2, 72, 0.0f, Grey, 120);
 	f28 = App->physics->AddCubeRotY(10, 1, 10, -10, 2, 74, 0.0f, Grey, 140);
@@ -67,15 +67,50 @@ bool ModuleSceneIntro::Start()
 	f38 = App->physics->AddCube(12, 1, 10, 23, 2, 44, 0.0f, Grey);
 	f39 = App->physics->AddCubeRotY(10, 1, 10, 15, 2, 43, 0.0f, Grey, 160);
 	f40 = App->physics->AddCubeRotY(10, 1, 10, 12, 2, 41, 0.0f, Grey, 140);
-	f41 = App->physics->AddCubeRotY(10, 1, 10, 11, 2, 39, 0.0f, Grey, 120);
+	f41 = App->physics->AddCubeRotY(10, 1, 10, 10, 2, 39, 0.0f, Grey, 100);
 
 	//WALLS
-	w1 = App->physics->AddCube(2, 6, 95, 15, 2, -11, 0.0f, Blue);
+	w1 = App->physics->AddCube(2, 6, 96, 15, 2, -11, 0.0f, Blue);
 	w2 = App->physics->AddCubeRotY(2, 6, 18, 8, 2, -64, 0.0f, Blue, 50);
 	w3 = App->physics->AddCube(15, 6, 2, -5, 2, -69, 0.0f, Blue);
 	w4 = App->physics->AddCubeRotY(2, 6, 18, -18, 2, -64, 0.0f, Blue, 130);
 	w5 = App->physics->AddCube(2, 6, 17, -24, 2, -50, 0.0f, Blue);
+	w6 = App->physics->AddCubeRotY(2, 6, 10, -27, 2, -38, 0.0f, Blue, 140);
+	w7 = App->physics->AddCube(16, 6, 2, -38, 2, -34, 0.0f, Blue);
+	w8 = App->physics->AddCubeRotY(2, 6, 16, -51, 2, -29, 0.0f, Blue, 140);
+	w9 = App->physics->AddCube(2, 6, 43, -56, 2, -2, 0.0f, Blue);
+	w10 = App->physics->AddCubeRotY(2, 6, 18, -49, 2, 25, 0.0f, Blue, 60);
+	w11 = App->physics->AddCube(16, 6, 2, -33, 2, 29, 0.0f, Blue);
+	w12 = App->physics->AddCubeRotY(2, 6, 8, -23, 2, 31, 0.0f, Blue, 55);
+	w13 = App->physics->AddCube(2, 6, 40, -20, 2, 53, 0.0f, Blue);
+	w14 = App->physics->AddCubeRotY(2, 6, 20, -12, 2, 78, 0.0f, Blue, 60);
+	w15 = App->physics->AddCube(37, 6, 2, 14, 2, 83, 0.0f, Blue);
+	w16 = App->physics->AddCubeRotY(2, 6, 19, 38, 2, 76, 0.0f, Blue, 140);
+	w17 = App->physics->AddCube(2, 6, 23, 44.5, 2, 59, 0.0f, Blue);
+	w18 = App->physics->AddCubeRotY(2, 6, 17, 38, 2, 43, 0.0f, Blue, 50);
+	w19 = App->physics->AddCube(18, 6, 2, 23, 2, 38, 0.0f, Blue);
 
+	w20 = App->physics->AddCube(2, 6, 93, 4, 2, -5, 0.0f, Blue);
+	w21 = App->physics->AddCubeRotY(2, 6, 8, 2, 2, -54, 0.0f, Blue, 40);
+	w22 = App->physics->AddCube(8, 6, 2, -4, 2, -58, 0.0f, Blue);
+	w23 = App->physics->AddCubeRotY(8, 6, 2, -10, 2, -56, 0.0f, Blue, 210);
+	w24 = App->physics->AddCube(2, 6, 20, -13, 2, -44, 0.0f, Blue);
+	w25 = App->physics->AddCubeRotY(2, 6, 16, -18, 2, -29, 0.0f, Blue, 135);
+	w26 = App->physics->AddCube(18, 6, 2, -32, 2, -24, 0.0f, Blue);
+	w27 = App->physics->AddCubeRotY(2, 6, 8, -43, 2, -22, 0.0f, Blue, 140);
+	w28 = App->physics->AddCube(2, 6, 35, -45, 2, -2, 0.0f, Blue);
+	w29 = App->physics->AddCubeRotY(2, 6, 6, -43, 2, 17, 0.0f, Blue, 60);
+	w30 = App->physics->AddCube(21, 6, 2, -30, 2, 19, 0.0f, Blue);
+	w31 = App->physics->AddCubeRotY(2, 6, 16, -14, 2, 24, 0.0f, Blue, 45);
+	w32 = App->physics->AddCube(2, 6, 39, -8, 2, 48, 0.0f, Blue);
+	w33 = App->physics->AddCubeRotY(2, 6, 8, -5, 2, 69, 0.0f, Blue, 60);
+	w34 = App->physics->AddCube(31, 6, 2, 13, 2, 71, 0.0f, Blue);
+	w35 = App->physics->AddCubeRotY(2, 6, 8, 30, 2, 69, 0.0f, Blue, 140);
+	w36 = App->physics->AddCube(2, 6, 17, 32, 2, 59, 0.0f, Blue);
+	w37 = App->physics->AddCubeRotY(2, 6, 4, 31, 2, 50, 0.0f, Blue, 50);
+	w38 = App->physics->AddCube(16, 6, 2, 22, 2, 49, 0.0f, Blue);
+	w39 = App->physics->AddCubeRotY(2, 6, 15, 9, 2, 45, 0.0f, Blue, 50);
+	
 	//start wall
 	//wstart= App->physics->AddCube(12, 3, 1, 10, 3, 13, 0.0f, Blue);
 
@@ -167,6 +202,41 @@ update_status ModuleSceneIntro::Update(float dt)
 	w3.Render();
 	w4.Render();
 	w5.Render();
+	w6.Render();
+	w7.Render();
+	w8.Render();
+	w9.Render();
+	w10.Render();
+	w11.Render();
+	w12.Render();
+	w13.Render();
+	w14.Render();
+	w15.Render();
+	w16.Render();
+	w17.Render();
+	w18.Render();
+	w19.Render();
+	w20.Render();
+	w21.Render();
+	w22.Render();
+	w23.Render();
+	w24.Render();
+	w25.Render();
+	w26.Render();
+	w27.Render();
+	w28.Render();
+	w29.Render();
+	w30.Render();
+	w31.Render();
+	w32.Render();
+	w33.Render();
+	w34.Render();
+	w35.Render();
+	w36.Render();
+	w37.Render();
+	w38.Render();
+	w39.Render();
+	
 
 	//wall start
 	wstart.Render();
--- RACING GAME/ModuleSceneIntro.h
+++ RACING GAME/ModuleSceneIntro.h
@@ -43,6 +43,10 @@ public:
 	//WALLS
 	Cube w1, w2, w3, w4, w5, w6, w7, w8, w9, w10;
 	Cube w11, w12, w13, w14, w15, w16, w17, w18, w19, w20;
+	Cube w21, w22, w23, w24, w25, w26, w27, w28, w29, w30;
+	Cube w31, w32, w33, w34, w35, w36, w37, w38, w39, w40;
+	Cube w41, w42, w43, w44, w45, w46, w47, w48, w49, w50;
+	Cube w51, w52, w53, w54, w55, w56, w57, w58, w59, w60;
 	Cube wstart;
 
 	int lap1 = 0;
