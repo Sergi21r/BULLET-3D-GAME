85927be901500ac7de3e83c142579e159b2f12c1
Sergi21r <sergi21r@gmail.com>
2018-01-06 21:15:03 +0100

    cleaning code

--- RACING GAME/ModuleSceneIntro.h
+++ RACING GAME/ModuleSceneIntro.h
@@ -45,8 +45,6 @@ public:
 	Cube w11, w12, w13, w14, w15, w16, w17, w18, w19, w20;
 	Cube w21, w22, w23, w24, w25, w26, w27, w28, w29, w30;
 	Cube w31, w32, w33, w34, w35, w36, w37, w38, w39, w40;
-	Cube w41, w42, w43, w44, w45, w46, w47, w48, w49, w50;
-	Cube w51, w52, w53, w54, w55, w56, w57, w58, w59, w60;
 	Cube wstart;
 
 	int lap1 = 0;
